312e8658c58253ada324cefb506c2bf084638ac1
Mario Mainardes <mainardes.m@gmail.com>
2026-02-24 12:24:11 -0300

    instalado o webpack

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Hello World2");
 });
 
+app.get("/teste", (req, res) => {
+  res.send("Hello World22224");
+});
+
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
 });
