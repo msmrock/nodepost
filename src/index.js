94fd3487a78e8709cf8f9011c28f17be316df4fa
Mario Mainardes <mainardes.m@gmail.com>
2026-02-24 22:04:35 -0300

    criado rotas

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
-import express from "express";
+import * as express from "express";
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World2");
+  res.send("GET /");
 });
 
-app.get("/teste", (req, res) => {
-  res.send("Hello World22224");
+app.post("/", (req, res) => {
+  res.status(201).send("POST /");
+});
+
+app.put("/", (req, res) => {
+  res.send("PUT /");
+});
+
+app.delete("/", (req, res) => {
+  res.send("DELETE /");
+});
+
+app.patch("/", (req, res) => {
+  res.send("PATCH /");
 });
 
 app.listen(3000, () => {
